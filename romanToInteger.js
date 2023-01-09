a03f6f38da9db725683cb7ceff0c8b2137e20a98
Lauren Sousa <thelaurensousa@gmail.com>
2023-01-09 10:58:26 -0500

    romanToInteger

---
+++ romanToInteger.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    var pairings = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 }
+    var answer = 0;
+    for(i=0; i<s.length; i++){
+        if(pairings[s[i]] < pairings[s[i+1]]){
+            answer -= pairings[s[i]];
+        } else {
+            answer += pairings[s[i]];
+        }
+    }
+    return answer
+};
