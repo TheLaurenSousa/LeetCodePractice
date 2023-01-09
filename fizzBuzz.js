d7654cd428a3508885eaabb0f15bd16701135fd2
Lauren Sousa <thelaurensousa@gmail.com>
2023-01-09 13:30:20 -0500

    fizzBuzz

---
+++ fizzBuzz.js
@@ -0,0 +1,19 @@
+var n = 3;
+
+var fizzBuzz = function(n) {
+    var output = [];
+    for (i=1; i<=n; i++){
+        if(i%3 === 0 && i%5 === 0){
+            output.push("FizzBuzz");
+        } else if (i%3 === 0){
+            output.push("Fizz");
+        } else if (i%5 === 0){
+            output.push("Buzz");
+        } else{
+            output.push(i.toString());
+        }
+    }
+    return output
+};
+
+console.log(fizzBuzz(n));
