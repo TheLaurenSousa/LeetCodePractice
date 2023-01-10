6c027e2dbc54abb30f7435088855ddc2e24ea311
Lauren Sousa <thelaurensousa@gmail.com>
2023-01-10 10:48:22 -0500

    TwoSum

---
+++ TwoSum.js
@@ -0,0 +1,16 @@
+var nums = [2,7,11,15];
+var target = 9;
+
+var twoSum = function(nums, target) {
+    for(i=0; i<nums.length; i++){
+        for(j=i+1; j<nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                let output = [i,j];               
+                return output; 
+            }
+                
+        }
+    }
+};
+
+console.log(twoSum(nums, target));
