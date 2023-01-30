405e633b8d7647e94be313e7814e9467ac02007e
Lauren Sousa <thelaurensousa@gmail.com>
2023-01-30 14:55:01 -0500

    lowestCommonAncestorOfABST

---
+++ lowestCommonAncestorOfABST.js
@@ -0,0 +1,5 @@
+var lowestCommonAncestor = function(root, p, q) {
+    if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+    if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+    return root;
+};
