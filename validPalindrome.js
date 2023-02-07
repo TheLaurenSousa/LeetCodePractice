159684eef8803aeac732fa8bfcf055544d048901
Lauren Sousa <thelaurensousa@gmail.com>
2023-02-07 09:53:22 -0500

    validPalindrome

--- validPalindrome.js
+++ validPalindrome.js
@@ -2,9 +2,7 @@ var s = "A man, a plan, a canal: Panama";
 
 var isPalindrome = function(s){
     s = s.toLowerCase();
-    console.log(s);
     s = s.replace(/[^A-Za-z0-9]/g, '');
-    console.log(s);
     var start = 0;
     var end = s.length-1;
     while(start < end){
